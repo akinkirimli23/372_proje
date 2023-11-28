65cac36d7ed8f7b83fa71e4ebcc46399303db64b
Akın KIRIMLI <akinkirimli20000@gmail.com>
2023-11-29 02:57:36 +0300

    akın baba html css

--- NERO/idari_personel.php
+++ NERO/idari_personel.php
@@ -76,100 +76,105 @@ $conn->close();
                         <li> <a href="veli.php">Velilerimiz</a></li>
                         <li> <a href="mezunlar.php">Mezunlarımız</a></li>
                         <li> <a href="Ogretmen.php">Öğretmen</a></li>
-                        <li> <a href="ogretmen_part_time.php">Öğretmen part time</a></li>
                         <li> <a href="ogrenci.php">Öğrenci</a></li>
                         <li> <a href="idari_personel.php">İdari Personel</a></li>
-                        <li> <a href="idari_personel_part_time.php">idari Personel Part Time</a></li>
+                      
                         <li> <a href="temizlik_gorevlisi.php">Temizlik Görevlisi</a></li>
                         <li> <a href="dersler.php">Dersler</a></li>
-                        <li> <a href="giderler.php">Giderler</a></li>    
+                        <li> <a href="giderler.php">Giderler</a></li>
+                        <li> <a href="malzemeler.php">Malzemeler</a></li>         
+
                     </ul>
             </div>
         </nav>
 
-      
+        <h1>İdari Personel</h1>
     </section>
 
+    </body>
+
+    <section class="sekmeler-us">
 
         <section class="blog-content">
-
-        <h1>İdari Personel</h1>
-        
-        </section>
-
-</body>
-
-
-
-            
-            <section class="contact-us">
-    
+      
+       
+        <h2>İdare Personel İnsert  </h2>
+            <br>
+               <br>
+  
+  
     
 
 <div class="contact-col">
     <form method="POST" action="idari_personel_insert.php" enctype="multipart/form-data">
         
         
-        <label for="isim">Ad:</label>
+        <label for="isim"><b>Ad:</b></label>
         <input type="text" name="isim" id="isim" placeholder="Lütfen adınızı giriniz" required>
 
-        <label for="soy_isim">Soyad:</label>
+        <label for="soy_isim"><b>Soyad:</b></label>
         <input type="text" name="soy_isim" id="soy_isim" placeholder="Lütfen soyadınızı giriniz" required>
 
-        <label for="yaş">Yaş:</label>
+        <label for="yaş"><b>Yaş:</b></label>
         <input type="number" name="yaş" id="yaş" placeholder="Lütfen yaşınızı giriniz" required>
 
-        <label for="cinsiyet">Cinsiyet:</label>
+        <label for="cinsiyet"><b>Cinsiyet:</b></label>
         <select name="cinsiyet" id="cinsiyet" required>
-            <option value="erkek">Erkek</option>
-            <option value="kadin">Kadın</option>
+            <option value="erkek"><b>Erkek</b></option>
+            <option value="kadin"><b>Kadın</b></option>
             <!-- Diğer cinsiyet seçenekleri ekleyebilirsiniz -->
         </select>
         <br>
         <br>
-        <label for="adres">Adres:</label>
+        <label for="adres"><b>Adres:</b></label>
     <textarea rows="4" name="adres" id="adres" placeholder="Lütfen adresinizi giriniz" required></textarea>
 
-    <label for="telefon">Telefon:</label>
+    <label for="telefon"><b>Telefon:</b></label>
     <input type="tel" name="telefon" id="telefon" placeholder="Lütfen telefon numaranızı giriniz" required>
 
-    <label for="maaş">Maaş:</label>
+    <label for="maaş"><b>Maaş:</b></label>
     <textarea rows="4" name="maaş" id="maaş" placeholder="Lütfen maaş giriniz" required></textarea>
 
-    <label for="çalışma_durumu">Çalışma Durumu:</label>
+    <label for="çalışma_durumu"><b>Çalışma Durumu:</b></label>
     <input type="text" name="çalışma_durumu" id="çalışma_durumu" placeholder="Lütfen çalışma durumu giriniz" required>
 
         <br>
-        <br>
+       
         <!--textarea rows="8" name="mesaj" id="mesaj" placeholder="Mesajınızı buraya giriniz" required></textarea-->
 
-        <button type="submit" class="hero-btn red-btn">Gönder</button> 
+        <button type="submit" class="hero-btn red-btn"><b>Gönder</b></button> 
     </form>
 </div>
 
+<br>
+<br>
+
+     
+<h2>İdare Personel Müsaitlik </h2>
+          <br>
+          <br>
 <div class="contact-col">
     <form method="POST" action="parttimeidari_personel_gün_ve_saat_insert.php" enctype="multipart/form-data">
      
 
     
-        <label for="idari_personel_id">İdari Personel ID:</label>
+        <label for="idari_personel_id"><b>İdari Personel ID:</b></label>
         <input type="number" name="idari_personel_id" id="idari_personel_id" placeholder="Lütfen İdari Personel ID giriniz" required>
 
-        <label for="gün_adı">Gün Adı:</label>
+        <label for="gün_adı"><b>Gün Adı:</b></label>
         <input type="text" name="gün_adı" id="gün_adı" placeholder="Lütfen Gün Adı giriniz" required>
 
-        <label for="başlangıç_saat">Başlangıç Saat:</label>
+        <label for="başlangıç_saat"><b>Başlangıç Saat:</b></label>
         <input type="time" name="başlangıç_saat" id="başlangıç_saat" placeholder="Lütfen Başlangıç Saat giriniz" required>
 
-        <label for="bitiş_saati">Bitiş Saati:</label>
+        <label for="bitiş_saati"><b>Bitiş Saati:</b></label>
         <input type="time" name="bitiş_saati" id="bitiş_saati" placeholder="Lütfen Bitiş Saati giriniz" required>
 
-        
         <br>
-        <br>
+       
         <!--textarea rows="8" name="mesaj" id="mesaj" placeholder="Mesajınızı buraya giriniz" required></textarea-->
 
-        <button type="submit" class="hero-btn red-btn">Gönder</button> 
+        <button type="submit" class="hero-btn red-btn"><b>Gönder</b></button> 
     </form>
 </div>
 
@@ -183,8 +188,7 @@ $conn->close();
         <h1>İdari Personel Verileri</h1>
         <br>
     </div>
-    <body style="background-color:lightgray;">
-    
+    <body style="background:powderblue;">    
 	<table align="center" border="1px" style="width=100%; line-height:40px;"> 
 	<tr> 
 		
@@ -216,7 +220,7 @@ $conn->close();
             echo "<td>" . $row["telefon"] . "</td>";
             echo "<td>" . $row["maaş"] . "</td>";
             echo "<td>" . $row["çalışma_durumu"] . "</td>";
-            echo "<td><a href='idari_personel_delete.php?idari_personel_id=" . $row["idari_personel_id"] . "' onclick=\"return confirm('Are you sure?')\">Delete</a></td>";
+            echo "<td><a href='idari_personel_delete.php?idari_personel_id=" . $row["idari_personel_id"] . "' onclick=\"return confirm('Are you sure?')\">X</a></td>";
 
 
             echo "</tr>";
