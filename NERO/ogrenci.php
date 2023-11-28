ddcb8e4c1cd779cf01e6cfc4313390ab761b0802
Akın KIRIMLI <akinkirimli20000@gmail.com>
2023-11-28 21:28:12 +0300

    baya bişy var

--- NERO/ogrenci.php
+++ NERO/ogrenci.php
@@ -131,14 +131,17 @@ $conn->close();
             <div class="nav-links">
                     <ul>
                     <li> <a href="index.html">HOME</a></li>
-                                                <li> <a href="veli.php">Velilerimiz</a></li>
-                                                <li> <a href="mezunlar.php">Mezunlarımız</a></li>
-                                                <li> <a href="Ogretmen.php">Öğretmen</a></li>
-                                               
-                                                <li> <a href="ogrenci.php">Öğrenci</a></li>
-                                                <li> <a href="idari_personel.php">İdari Personel</a></li>
-                                                <li> <a href="temizlik_gorevlisi.php">Temizlik Görevlisi</a></li>
-                                                <li> <a href="dersler.php">Dersler</a></li>
+                        <li> <a href="veli.php">Velilerimiz</a></li>
+                        <li> <a href="mezunlar.php">Mezunlarımız</a></li>
+                        <li> <a href="Ogretmen.php">Öğretmen</a></li>
+                        <li> <a href="ogretmen_part_time.php">Öğretmen part time</a></li>
+                        <li> <a href="ogrenci.php">Öğrenci</a></li>
+                        <li> <a href="idari_personel.php">İdari Personel</a></li>
+                        <li> <a href="idari_personel_part_time.php">idari Personel Part Time</a></li>
+                        <li> <a href="temizlik_gorevlisi.php">Temizlik Görevlisi</a></li>
+                        <li> <a href="dersler.php">Dersler</a></li>
+                        <li> <a href="giderler.php">Giderler</a></li>    
+
 
                     </ul>
             </div>
